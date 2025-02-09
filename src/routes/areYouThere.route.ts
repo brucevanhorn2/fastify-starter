a4d466c39ffc7d92440027a271e6c54a8b0b69c7
Bruce Van Horn <bvanhorn@cleartechnologies.net>
2025-02-09 00:23:43 -0600

    renamed mislabelled controller

--- src/routes/areYouThere.route.ts
+++ src/routes/areYouThere.route.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from 'fastify';
-import { areYouThereController } from '../controllers/areYouThereController';
+import { areYouThereController } from '../controllers/areYouThere.controller';
 
 export default async function areYouThereRoutes(fastify: FastifyInstance) {
   fastify.get('/api/v1/are-you-there', areYouThereController);
