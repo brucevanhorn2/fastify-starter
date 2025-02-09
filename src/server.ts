fe793a2040ab0f0d069b6cfde3c4da98eb1edfd6
Bruce Van Horn <bvanhorn@cleartechnologies.net>
2025-02-09 00:58:29 -0600

    clean up.  use config for host and port.

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import Fastify from 'fastify';
 import helloRoutes from './routes/hello.route';
+import areYouThereRoutes from './routes/areYouThere.route';
+
 
 const fastify = Fastify({ logger: true });
+const PORT = parseInt(process.env.PORT ?? '3000', 10);
+const HOST = process.env.BIND ?? '0.0.0.0';
 
 fastify.register(helloRoutes);
+fastify.register(areYouThereRoutes);
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000, host: '0.0.0.0' });
-    console.log(`🚀 Server running at http://localhost:3000`);
+    await fastify.listen({ port: PORT, host: HOST });
+    console.log(`🚀 Server running at http://${HOST}:${PORT}`);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
